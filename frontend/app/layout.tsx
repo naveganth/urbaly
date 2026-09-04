26a9c3956c6d8c6932039094d62d50534782f96d
naveganth <lucas.navegant@gmail.com>
2026-09-04 10:21:12 -0300

    Add shadcn preset

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,16 @@
-import { Provider } from "@/components/ui/provider"
+import "./globals.css";
+import { Inter, Instrument_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const instrumentSansHeading = Instrument_Sans({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html suppressHydrationWarning lang="en">
-      <body>
-        <Provider>{children}</Provider>
-      </body>
+    <html lang="es" className={cn("font-sans", inter.variable, instrumentSansHeading.variable)}>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
